a01659c6a10649b50273d557b0a8a0052448afd6
fauzi <sifauzi@gmail.com>
2019-04-24 07:26:37 +0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 
 function getComponent() {
-    return
+    return import('lodash').then({ default: _ }) ={
+        
+    }
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     btn.innerHTML = 'Clicke me man';
     btn.onclick = printme;
     element.appendChild(btn);
-    
+
     return element;
 }
 
 document.body.appendChild(component());
 
 
-if(module.hot){
-    module.hot.accept('./print.js', function(){
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
         console.log('Accepting the updated printMe module');
         printme();
     })
